9eb42a865e17205c0d14abc5022f66381e971a3e
Abel <abelabelao@hotmail.com.br>
2024-10-15 11:40:39 -0300

    Exercício lab-padroes-projeto-spring

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableFeignClients
+@SpringBootApplication
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Service
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+}
